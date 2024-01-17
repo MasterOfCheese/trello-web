d972d5ffc008d3d4f97ebc4cea42689131b02b03
Nam Phuong <pnam24823@gmail.com>
2024-01-17 16:24:00 +0700

    Dark Light P1

--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,19 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-import { pink } from '@mui/material/colors'
-
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { cyan, deepOrange, orange, red } from '@mui/material/colors'
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'light', //default is light
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: red,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: pink[300]
-    },
-    test: {
-      primary: red[500]
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
     }
   }
 })
