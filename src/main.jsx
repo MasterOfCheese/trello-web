96a648dcd950bfe8ad866c8c5dbcfe21d5de1a63
Nam Phuong <pnam24823@gmail.com>
2024-01-23 00:18:22 +0700

    Basic Layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import { ThemeProvider } from '@mui/material/styles'
+// import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
 
 
