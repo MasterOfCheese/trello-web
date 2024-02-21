cd7907582badd6d613480065a0b9c4460d5802a8
Nam Phuong <pnam24823@gmail.com>
2024-02-21 20:01:05 +0700

    mock_sord_data

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -1,11 +1,5 @@
 // Sort Columns/Cards
 export const mapOrder = (originalArray, orderArray, key) => {
   if (!originalArray || !orderArray || !key) return []
-
-  const clonedArray = [...originalArray]
-  const orderedArray = clonedArray.sort((a, b) => {
-    return orderArray.indexOf(a[key]) - orderArray.indexOf(b[key])
-  })
-
-  return orderedArray
+  return [...originalArray].sort((a, b) => orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]))
 }
