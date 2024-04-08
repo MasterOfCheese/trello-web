42f2527c4b0e58070918554347219f7ea39d29cb
Nam Phuong <pnam24823@gmail.com>
2024-04-08 14:15:35 +0700

    fix_bug_37.2

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -5,3 +5,18 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
+
+
+/*
+Hàm generatePlaceholderCard: Xử lý bug logic thư viện Dnd-kit khi Column rỗng
+FE_PlaceholderCard chỉ để xử lý giữ chỗ card -> fix bug empty card trên FrontEnd
+chứ không liên quan đến BackEnd
+*/
+export const generatePlaceholderCard = (column) => {
+  return {
+    _id: `${column.id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._columnId,
+    FE_PlaceholderCard: true
+  }
+}
