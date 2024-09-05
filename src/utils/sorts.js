4f5683d6e769aaec526f7a618a6c78038cb64121
Nam Phuong <pnam24823@gmail.com>
2024-09-05 14:50:17 +0700

    testReview

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -1,5 +1,5 @@
 // Sort Columns/Cards
 export const mapOrder = (originalArray, orderArray, key) => {
-  if (!originalArray || !orderArray || !key) return []
+  if ( !originalArray || !orderArray || Array.isArray(key) ) return []
   return [...originalArray].sort((a, b) => orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]))
 }
