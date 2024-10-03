ab995bbd76ba26d302869631087ae2810e190e41
trinhduong <you@example.com>
2024-10-03 15:40:56 +0700

    backCode

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -3,3 +3,4 @@ export const mapOrder = (originalArray, orderArray, key) => {
   if (!originalArray || !orderArray || !key) return []
   return [...originalArray].sort((a, b) => orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]))
 }
+//today is 10/0/2024
